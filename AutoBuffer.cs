d8cc42b02e5da0ea6a9f657d82f1ca4c6ba550d3
Isaac Paul <izackp@gmail.com>
2017-08-26 20:13:42 -0400

    Fixed arrays not being serialized correctly

--- AutoBuffer.cs
+++ AutoBuffer.cs
@@ -75,7 +75,7 @@ namespace AutoBuffer {
                 type = type.GetGenericTypeDefinition();
             else if (type.IsEnum)
                 type = Enum.GetUnderlyingType(type);
-            return _typeCacheFactory.GetInstance(type);
+            return _typeCacheFactory.GetOrBuildInstance(type);
         }
 
         public EntityMapper GetEntityMapper(Type type, bool build = true) {
